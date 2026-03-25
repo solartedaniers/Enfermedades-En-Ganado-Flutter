81aacc224d318f1099c067d89013b66409d4f675
Juan Felipe Mora <juan.morarevel@campusucc.educo>
2026-03-25 14:56:09 -0500

    Traduje la pantalla del diagnostico

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -3,11 +3,17 @@ import 'package:flutter/services.dart';
 
 class AppStrings {
   static Map<String, dynamic> _strings = {};
+  static String _currentLanguage = 'es';
 
   static Future<void> load(String lang) async {
     final json = await rootBundle.loadString('lib/l10n/$lang.json');
     _strings = jsonDecode(json);
+    _currentLanguage = lang;
   }
 
   static String t(String key) => _strings[key] ?? key;
+
+  static String get currentLanguage => _currentLanguage;
+
+  static bool get isEnglish => _currentLanguage == 'en';
 }
